701f24e1877d66a32824e193454236f433d480c1
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-03 13:52:57 +0600

    updated user class

--- src/main/java/com/mursalin/chat_app/model/User.java
+++ src/main/java/com/mursalin/chat_app/model/User.java
@@ -2,16 +2,18 @@ package com.mursalin.chat_app.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
-//@Document
+@Document
 public class User {
 
-//    @Id
+    @Id
     private String userId;
     private String username;
+    private String userEmail;
+    private String password;
     private Status status;
 }
