db1d9b3fb49f857e962fabcb8b588a1f8c272091
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-05 01:00:56 +0600

    handling message request, sending via websocket, using chat key generator, fetching message history

--- src/main/java/com/mursalin/chat_app/service/imp/ChatRoomServiceImp.java
+++ src/main/java/com/mursalin/chat_app/service/imp/ChatRoomServiceImp.java
@@ -1,12 +1,23 @@
 package com.mursalin.chat_app.service.imp;
 
+import com.mursalin.chat_app.dto.ChatMessageRequest;
 import com.mursalin.chat_app.model.ChatRoom;
+import com.mursalin.chat_app.model.Conversation;
 import com.mursalin.chat_app.repository.ChatRoomRepository;
 import com.mursalin.chat_app.service.ChatRoomService;
+import com.mursalin.chat_app.utils.ChatKeyGenerator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.broker.SimpleBrokerMessageHandler;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -14,16 +25,36 @@ import java.util.List;
 public class ChatRoomServiceImp implements ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
-    private final MongoService mongoService;
+    private final MongoTemplate mongoTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @Override
-    public ChatRoom privateMessageRequest(ChatRoom chatRoom) {
+    public void privateMessageRequest(ChatMessageRequest chatMessageRequest) {
+        String chatRoomId = chatMessageRequest.getChatRoomId();
 
-        return chatRoomRepository.save(chatRoom);
+        Query query = new Query(Criteria.where("_id").is(chatRoomId));
+
+        Update update = new Update()
+                .push("conversations", chatMessageRequest.getMessageData());
+
+        mongoTemplate.updateFirst(query, update, ChatRoom.class);
+        messagingTemplate.convertAndSend("/chatroom/" + chatRoomId, chatMessageRequest.getMessageData());
     }
 
     @Override
-    public List<ChatRoom> getAllPrivateMessages(String senderId, String receiverId) {
-        return mongoService.getChatsBetweenUsers(senderId, receiverId);
+    public ChatRoom getAllPrivateMessages(List<String> membersId) {
+        String chatKey = ChatKeyGenerator.generate(membersId);
+        ChatRoom existingChatRoom = chatRoomRepository.findByChatKey(chatKey);
+        if (existingChatRoom != null) {
+            existingChatRoom.getConversations().sort(Comparator.comparing(Conversation::getSendAt));
+            return existingChatRoom;
+        }
+
+        ChatRoom newRoom = new ChatRoom();
+        newRoom.setMembersId(membersId);
+        newRoom.setChatKey(chatKey);
+        newRoom.setConversations(new ArrayList<>());
+        return chatRoomRepository.save(newRoom);
+
     }
 }
