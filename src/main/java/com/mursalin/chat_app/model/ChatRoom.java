53c3501168bb90e1bb8101324886ba0b4c09ab8b
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-05 01:03:02 +0600

    new variable chatkey used, removed older variables , introduced new one for better functionality, setter method introduced

--- src/main/java/com/mursalin/chat_app/model/ChatRoom.java
+++ src/main/java/com/mursalin/chat_app/model/ChatRoom.java
@@ -5,19 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document
 @Builder
 public class ChatRoom {
 
     @Id
     private String chatRoomId;
-    private String senderId;
-    private String receiverId;
-    private String message;
-    private LocalDateTime sendAt;
+    private String chatKey;
+    private List<String> membersId = new ArrayList<>();
+    private List<Conversation> conversations = new ArrayList<>();
+
+    public void setMemberId(String memberId) {
+        this.membersId.add(memberId);
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversations.add(conversation);
+    }
 }
