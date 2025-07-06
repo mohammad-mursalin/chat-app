260129bf564e957b9ce2d794f0502c08770cab1f
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-06 18:04:10 +0600

    service for fetching group names and member

---
+++ src/main/java/com/mursalin/chat_app/service/imp/MongoService.java
@@ -0,0 +1,34 @@
+package com.mursalin.chat_app.service.imp;
+
+import com.mursalin.chat_app.dto.GroupListResponse;
+import com.mursalin.chat_app.model.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class MongoService {
+
+    private final MongoTemplate mongoTemplate;
+
+    public List<GroupListResponse> getUserGroups(String currentUserId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("membersId").in(currentUserId));
+        query.addCriteria(Criteria.where("groupName").ne(null)); // group only
+
+        query.fields().include("groupName").include("membersId");
+
+        List<ChatRoom> chatRooms = mongoTemplate.find(query, ChatRoom.class);
+
+        return chatRooms.stream()
+                .map(room -> new GroupListResponse(room.getGroupName(), room.getMembersId()))
+                .collect(Collectors.toList());
+    }
+
+}
