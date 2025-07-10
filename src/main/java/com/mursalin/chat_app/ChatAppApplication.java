a02adb6910875019e1c1ad57bc61fc6aa42ab034
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-10 20:01:04 +0600

    enabling async

--- src/main/java/com/mursalin/chat_app/ChatAppApplication.java
+++ src/main/java/com/mursalin/chat_app/ChatAppApplication.java
@@ -2,8 +2,10 @@ package com.mursalin.chat_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ChatAppApplication {
 
 	public static void main(String[] args) {
