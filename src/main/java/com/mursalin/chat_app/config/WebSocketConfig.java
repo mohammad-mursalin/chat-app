9a9d8e1f4ed1a17f9ffcca5443f21a205348ea68
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-10 10:09:22 +0600

    updating the handshake url

--- src/main/java/com/mursalin/chat_app/config/WebSocketConfig.java
+++ src/main/java/com/mursalin/chat_app/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/homepage")
+        registry.addEndpoint("/handshake")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
