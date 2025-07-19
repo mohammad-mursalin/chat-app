c17bab7c4bd6ae67387eb6e208114c4754202d1b
mohammad-mursalin <mursalinlamon@gmail.com>
2025-07-19 21:29:29 +0600

    imports

--- src/main/java/com/mursalin/chat_app/model/User.java
+++ src/main/java/com/mursalin/chat_app/model/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
